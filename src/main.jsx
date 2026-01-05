185989aa13a9647ebbf981ca9d143dd685c3a034
kajal <kajalkumari800023@gmail.com>
2026-01-05 14:43:10 +0530

    Fix routing using HashRouter for GitHub Pages

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import { CartProvider } from "./context/CartContext";
+import { HashRouter } from "react-router-dom";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <CartProvider>
-      <App />
-    </CartProvider>
+    <HashRouter>
+      <CartProvider>
+        <App />
+      </CartProvider>
+    </HashRouter>
   </React.StrictMode>
 );
